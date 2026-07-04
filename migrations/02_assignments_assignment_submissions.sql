e4fd036507dfbd2fcdd3b7bb14f277781a56c9f9
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-07-03 21:41:22 -0700

    wip: setup assignments & assignment_submissions tables in DB

---
+++ migrations/02_assignments_assignment_submissions.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS assignment_submissions;
+DROP TABLE IF EXISTS assignments;
+
+CREATE TABLE assignments(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  content TEXT,
+  day INTEGER,
+  chapter INTEGER,
+  duration INTEGER
+);
+
+CREATE TABLE assignment_submissions(
+  id SERIAL PRIMARY KEY,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER NOT NULL,
+  duration INTEGER,
+  submission_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
