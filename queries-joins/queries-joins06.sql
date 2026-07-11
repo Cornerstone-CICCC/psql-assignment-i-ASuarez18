bf38219fcf45346c6d08175c89b71f7925bd74c4
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-07-10 23:19:03 -0700

    query: queries-join_06 finished (passed)

--- queries-joins/queries-joins06.sql
+++ queries-joins/queries-joins06.sql
@@ -1,1 +1,6 @@
 -- Get total submissions enrolled students
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions 
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name;
