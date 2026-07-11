53c84b1f572f67fc187dd74c41d0ddbcbf4d6d62
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-07-10 23:40:46 -0700

    query: queries-group-by_03 finished (passed)

--- queries-group-by/queries-group-by03.sql
+++ queries-group-by/queries-group-by03.sql
@@ -1,1 +1,7 @@
 -- Get classes more 18 students
+SELECT c.name AS class_name, count(*) AS total_students
+FROM classes c
+JOIN students s ON c.id = s.class_id
+GROUP BY class_name
+HAVING count(*) >= 18
+ORDER BY total_students;
