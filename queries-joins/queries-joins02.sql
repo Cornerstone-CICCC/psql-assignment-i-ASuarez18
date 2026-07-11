3916c85c7937c8616291284c231f074be98d86e2
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-07-10 20:19:19 -0700

    query: queries-join_02 finished (passed)

--- queries-joins/queries-joins02.sql
+++ queries-joins/queries-joins02.sql
@@ -1,1 +1,5 @@
 -- Get enrolled students with class name
+SELECT s.name AS student_name, s.email, c.name AS class_name
+FROM students s
+JOIN classes c ON s.class_id = c.id
+WHERE s.end_date IS NULL;
