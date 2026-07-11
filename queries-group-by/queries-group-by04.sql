a1cf419e2afd979e98b41d56cd7980581ad2ceae
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-07-10 23:45:37 -0700

    query: queries-group-by_04 finished (passed)

--- queries-group-by/queries-group-by04.sql
+++ queries-group-by/queries-group-by04.sql
@@ -1,1 +1,7 @@
 -- Get total submissions per class
+SELECT c.name as class_name, count(*) as total_submissions 
+FROM classes c
+JOIN students s ON c.id = s.class_id
+JOIN assignment_submissions asb ON s.id = asb.student_id
+GROUP BY class_name
+ORDER BY total_submissions DESC;
