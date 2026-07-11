c20eb3e2c41323de43158c6e0fa2024c96a6a701
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-07-10 23:25:02 -0700

    query: queries-join_07 finished (passed)

--- queries-joins/queries-joins07.sql
+++ queries-joins/queries-joins07.sql
@@ -1,1 +1,7 @@
 -- Get total submissions enrolled students less 100
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions 
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING count(assignment_submissions.*) < 100;
