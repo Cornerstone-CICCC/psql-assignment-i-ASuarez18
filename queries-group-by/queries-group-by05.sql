3415107af6eb32860862e1e61f735d3192908954
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-07-10 23:52:38 -0700

    query: queries-group-by_05 finished (passed)

--- queries-group-by/queries-group-by05.sql
+++ queries-group-by/queries-group-by05.sql
@@ -1,1 +1,7 @@
 -- Get enrolled students average time completion
+SELECT s.name as student_name, AVG(duration) as average_time
+FROM students s
+JOIN assignment_submissions asb ON s.id = asb.student_id
+WHERE s.end_date IS NULL
+GROUP BY student_name
+ORDER BY average_time DESC;
