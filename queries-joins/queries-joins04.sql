1be54cf5b572458a27908f3914b3f55472a9b02c
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-07-10 20:37:45 -0700

    query: queries-join_04 finished (passed)

--- queries-joins/queries-joins04.sql
+++ queries-joins/queries-joins04.sql
@@ -1,1 +1,6 @@
 -- Get total time assignments per student
+SELECT s.name AS student_name, SUM(a.duration) AS total_time
+FROM students s
+JOIN assignment_submissions a ON s.id = a.student_id
+WHERE s.name = 'Ibrahim Schimmel'
+GROUP BY s.name;
