2a4461ea1a1759dd77c1361b496b45e3b5a9982f
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-07-11 23:56:36 -0700

    query: queries-in_01 finished (passed)

--- queries-in/queries-in01.sql
+++ queries-in/queries-in01.sql
@@ -1,1 +1,7 @@
 --  Get total assistance requests instructor
+SELECT COUNT(ar.id) as total_assistances, i.name as instructor_name
+FROM instructors i
+JOIN assistance_requests ar ON i.id = ar.instructor_id
+WHERE i.name = 'Waylon Boehm'
+GROUP BY instructor_name
+ORDER BY total_assistances DESC;
