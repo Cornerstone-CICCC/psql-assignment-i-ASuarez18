c875771b6480a05b519dea4c7311a0dbeea1ac9c
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-07-11 23:59:25 -0700

    query: queries-in02 finished (passed)

--- queries-in/queries-in02.sql
+++ queries-in/queries-in02.sql
@@ -1,1 +1,7 @@
 -- Get total assistance per student
+SELECT  COUNT(ar.id) AS total_assistances, s.name AS student_name
+FROM students s
+JOIN assistance_requests ar ON s.id = ar.student_id
+WHERE s.name = 'Elliot Dickinson'
+GROUP BY student_name
+ORDER BY total_assistances DESC;
