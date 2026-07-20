d027afd39ac10c4c8aefe2cdf2d63e33882a03a7
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-07-20 13:53:01 -0700

    query: queries-in10 finished (passed)

--- queries-in/queries-in10.sql
+++ queries-in/queries-in10.sql
@@ -1,1 +1,11 @@
 -- Get total assistance requests assignment
+SELECT 
+  a.id, 
+  a.day, 
+  a.chapter, 
+  a.name, 
+  COUNT(ar.id) AS total_assistances
+FROM assignments a
+LEFT JOIN assistance_requests ar ON a.id = ar.assignment_id
+GROUP BY a.id, a.name, a.day, a.chapter
+ORDER BY total_assistances DESC;
