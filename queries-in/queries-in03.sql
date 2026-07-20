3c9f9b23764e80d52b4b2e65f366d181f3514fdd
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-07-20 13:21:06 -0700

    query: queries-in03 finished (passed)

--- queries-in/queries-in03.sql
+++ queries-in/queries-in03.sql
@@ -1,1 +1,11 @@
 -- Get data assistance request
+SELECT 
+  i.name AS instructor, 
+  s.name AS student, 
+  a.name AS assignment,
+  (ar.completed_at - ar.started_at) AS assistance_duration
+FROM assistance_requests ar
+JOIN instructors i ON ar.instructor_id = i.id
+JOIN students s ON ar.student_id = s.id
+LEFT JOIN assignments a ON ar.assignment_id = a.id
+ORDER BY assistance_duration;
