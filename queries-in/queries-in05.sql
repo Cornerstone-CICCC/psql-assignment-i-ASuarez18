fe86a87da21363568697d837d39330ee19e12e77
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-07-20 13:37:25 -0700

    query: queries-in05 finished (passed)

--- queries-in/queries-in05.sql
+++ queries-in/queries-in05.sql
@@ -1,1 +1,9 @@
 -- Get avg duration assistance per class
+SELECT 
+  c.name AS class, 
+  AVG(ar.completed_at - ar.started_at) AS avg_duration_assistance
+FROM assistance_requests ar
+JOIN students s ON ar.student_id = s.id
+JOIN classes c ON s.class_id = c.id
+GROUP BY c.name
+ORDER BY avg_duration_assistance ASC;
