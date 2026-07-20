1af8f9f2e2fc5e5d0fba4e6cf3908d2f02bc923e
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-07-20 13:56:11 -0700

    query: queries-in12 finished (passed)

--- queries-in/queries-in12.sql
+++ queries-in/queries-in12.sql
@@ -1,1 +1,10 @@
 -- Get instructor assisted class
+SELECT DISTINCT 
+  i.name AS instructor, 
+  c.name AS class
+FROM instructors i
+JOIN assistance_requests ar ON i.id = ar.instructor_id
+JOIN students s ON ar.student_id = s.id
+JOIN classes c ON s.class_id = c.id
+WHERE c.name = 'JUL02'
+ORDER BY instructor;
