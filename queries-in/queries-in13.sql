65236084cf845b2e8e2106910b4f9b302841073b
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-07-20 13:57:56 -0700

    query: queries-in13 finished (passed)

--- queries-in/queries-in13.sql
+++ queries-in/queries-in13.sql
@@ -1,1 +1,12 @@
 -- Get instructors assisted class with number
+SELECT 
+  i.name AS instructor, 
+  c.name AS class,
+  COUNT(ar.id) AS total_assistances
+FROM instructors i
+JOIN assistance_requests ar ON i.id = ar.instructor_id
+JOIN students s ON ar.student_id = s.id
+JOIN classes c ON s.class_id = c.id
+WHERE c.name = 'JUL02'
+GROUP BY i.name, c.name
+ORDER BY instructor;
