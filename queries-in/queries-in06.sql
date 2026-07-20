198e811e56c5409c675e7a8f95017e301e8f1810
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-07-20 13:40:56 -0700

    query: queries-in06 finished (passed)

--- queries-in/queries-in06.sql
+++ queries-in/queries-in06.sql
@@ -1,1 +1,10 @@
 -- Get class longest assistance
+SELECT 
+  c.name AS class, 
+  AVG(ar.completed_at - ar.started_at) AS avg_duration_assistance
+FROM assistance_requests ar
+JOIN students s ON ar.student_id = s.id
+JOIN classes c ON s.class_id = c.id
+GROUP BY c.name
+ORDER BY avg_duration_assistance DESC
+LIMIT 1;
