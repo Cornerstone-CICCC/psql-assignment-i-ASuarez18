32acfa11c49f75ecd2278295964a7a2b92da43ca
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-07-20 13:49:37 -0700

    query: queries-in09 finished (passed)

--- queries-in/queries-in09.sql
+++ queries-in/queries-in09.sql
@@ -1,1 +1,11 @@
 -- Get avg total assistance requests class
+SELECT AVG(total_duration) AS avg_total_duration
+FROM (
+  SELECT 
+    c.name AS class, 
+    SUM(ar.completed_at - ar.started_at) AS total_duration
+  FROM assistance_requests ar
+  JOIN students s ON ar.student_id = s.id
+  JOIN classes c ON s.class_id = c.id
+  GROUP BY c.name
+) AS class_totals;
