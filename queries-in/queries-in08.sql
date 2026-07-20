6fff6e0d13815ab54897f717a0982721e3f0e4d4
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-07-20 13:47:19 -0700

    query: queries-in08 finished (passed)

--- queries-in/queries-in08.sql
+++ queries-in/queries-in08.sql
@@ -1,1 +1,9 @@
 -- Get total assistance requests class
+SELECT 
+  c.name AS class, 
+  SUM(ar.completed_at - ar.started_at) AS total_duration
+FROM assistance_requests ar
+JOIN students s ON ar.student_id = s.id
+JOIN classes c ON s.class_id = c.id
+GROUP BY c.name
+ORDER BY total_duration ASC;
